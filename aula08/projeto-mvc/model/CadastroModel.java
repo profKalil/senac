e5ae0a716e9005a61d86dede17e207bd5bcbda0d
profKalil <84177112+profKalil@users.noreply.github.com>
2024-04-17 01:10:29 -0300

    Create CadastroModel.java

---
+++ aula08/projeto-mvc/model/CadastroModel.java
@@ -0,0 +1,12 @@
+package model;
+
+public class CadastroModel {
+	private String nome;
+
+	public void setNomeModel(String nome) {
+		this.nome = nome;
+	}
+	public String getNomeModel() {
+		return this.nome;
+	}
+}
