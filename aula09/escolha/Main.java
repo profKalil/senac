a8b026eb1a16a6b4cddddfb8e85b22fd06cfb907
profKalil <84177112+profKalil@users.noreply.github.com>
2024-04-29 22:39:20 -0300

    Add files via upload

---
+++ aula09/escolha/Main.java
@@ -0,0 +1,12 @@
+package escolha;
+
+import escolha.controller.Controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Controller();
+
+	}
+
+}
---
+++ aula09/escolha/model/DestinoModel.java
@@ -0,0 +1,17 @@
+package escolha.model;
+
+public class DestinoModel {
+	private String nome;
+
+	public DestinoModel(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
